d42331ca47483cc5d16f7c6ea8330104f6e8f482
senntyou <jiangjinbelief@163.com>
2022-09-16 11:24:02 +0800

    add GlobalExceptionHandler

---
+++ pro-base/src/main/java/dr/scs/base/exception/ApiAssert.java
@@ -0,0 +1,14 @@
+package dr.scs.base.exception;
+
+import dr.scs.base.IErrorCode;
+
+// 断言处理类，用于抛出各种API异常
+public class ApiAssert {
+  public static void fail(String message) {
+    throw new ApiException(message);
+  }
+
+  public static void fail(IErrorCode errorCode) {
+    throw new ApiException(errorCode);
+  }
+}
